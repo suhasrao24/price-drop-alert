ed7f09ea27782325884f37b27aca7cf106a34a3b
suhasrao24 <83645129+suhasrao24@users.noreply.github.com>
2021-05-10 13:40:12 +0530

    Add files via upload

---
+++ price drop.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+from twilio.rest import Client
+import config
+
+URL = 'https://www.tpstech.in/products/hp-oa04-notebook-original-battery-f3b94aa-black'
+PRODUCT = 'TPS Laptop Battery'
+TARGET_PRICE = 3000
+account_sid = config.account_sid
+auth_token = config.auth_token
+FROM = config.FROM
+TO = config.TO
+
+
+response = requests.get(URL)
+result = response.text
+
+soup = BeautifulSoup(result, 'html.parser')
+price_data = soup.find(name='span', class_='product-single__price')
+price = float(price_data.get('content'))
+
+if price <= TARGET_PRICE:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body=f"\nPrice drop alert!! {PRODUCT} now costs {price}. Link: {URL}",
+        from_=FROM,
+        to=TO
+    )
+    print(message.status)
